58e266dad20a2c1c6c47f7c1a0ff0686809c4a98
Lucas Martins Lima <lucasmartiinslima@gmail.com>
2024-11-23 01:29:46 -0300

    add general imports

--- src/app/features/catalog/catalog.module.ts
+++ src/app/features/catalog/catalog.module.ts
@@ -1,11 +1,21 @@
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ProductService } from 'src/app/core/services/product.service';
 import { CatalogRoutingModule } from './catalog-routing.module';
 import { CatalogComponent } from './catalog.component';
 
 @NgModule({
   declarations: [CatalogComponent],
-  imports: [CommonModule, CatalogRoutingModule],
+  imports: [
+    CommonModule,
+    CatalogRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
   exports: [CatalogComponent],
+  providers: [ProductService],
 })
 export class CatalogModule {}
