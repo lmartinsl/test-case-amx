f5f94be006b3025707756570033e4431fb11cb95
Lucas Martins Lima <lucasmartiinslima@gmail.com>
2024-11-26 22:32:07 -0300

    remove default user service

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,18 +1,12 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
-import { ProfileEnum } from './core/enums/profile.enum';
 import { User } from './core/models/user.interface';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AppService {
-  subjectUser$ = new BehaviorSubject<User | undefined>({
-    userName: 'lucas',
-    password: 'lucas123',
-    name: 'Lucas Martins Lima',
-    profile: ProfileEnum.ADMIN,
-  });
+  subjectUser$ = new BehaviorSubject<User | undefined>(undefined);
 
   setCurrentUser(user: User): void {
     this.subjectUser$.next(user);
